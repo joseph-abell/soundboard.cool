04c54666fe8d903bd93e3e9bce7d13fcc15d9841
Joseph Abell <jsphabll@gmail.com>
2016-05-06 20:30:47 +0100

    moving everything back into the build folder, as it didn't help the build process.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: __dirname
+  build: path.join(__dirname, 'build')
 };
 
 process.env.BABEL_ENV = TARGET;
